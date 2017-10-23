8cd566fef3926a07757d59009a34233cbd175a8d
Adam <adamflorianizydorczyk@gmail.com>
2017-10-23 18:00:57 +0200

    data block creator improvements

--- src/main/java/tk/aizydorczyk/util/header/DataBlockCreator.java
+++ src/main/java/tk/aizydorczyk/util/header/DataBlockCreator.java
@@ -2,6 +2,7 @@ package tk.aizydorczyk.util.header;
 
 import org.apache.commons.lang3.reflect.FieldUtils;
 import tk.aizydorczyk.common.annotation.ExcelColumn;
+import tk.aizydorczyk.enums.DataBlockType;
 import tk.aizydorczyk.enums.Messages;
 import tk.aizydorczyk.model.Cell;
 import tk.aizydorczyk.model.DataBlock;
@@ -13,9 +14,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static tk.aizydorczyk.enums.Messages.MORE_FIELDS_THAN_HEADERS;
-import static tk.aizydorczyk.enums.Messages.MORE_HEADERS_THAN_FIELDS;
-import static tk.aizydorczyk.enums.Messages.LACK_OF_MAIN_HEADER;
+import static tk.aizydorczyk.enums.Messages.*;
 
 
 public class DataBlockCreator {
@@ -33,8 +32,14 @@ public class DataBlockCreator {
 				.orElseThrow(() -> new DataBlockCreateFail(LACK_OF_MAIN_HEADER));
 
 		return objects.stream()
-				.map(o -> createDataBlock(o, mainHeader.getBottomHeaders()))
+				.map(o -> mapToDataBlocks(o, mainHeader))
 				.collect(Collectors.toList());
+	}
+
+	private DataBlock mapToDataBlocks(Object o, Header mainHeader) {
+		DataBlock dataBlock = createDataBlock(o, mainHeader.getBottomHeaders());
+		dataBlock.setHeader(mainHeader);
+		return dataBlock;
 	}
 
 	private DataBlock createDataBlock(Object object, List<Header> headers) {
@@ -43,24 +48,28 @@ public class DataBlockCreator {
 
 		checkHeadersAndFieldsNumberEquality(fields.size(), headers.size());
 
-		for (int index = 0; index < headers.size(); index++) {
+		for (int index = 0; index < headers.size() && index < fields.size(); index++) {
 			Field field = fields.get(index);
 			Header header = headers.get(index);
 
-			if (header.isOverCollection() && header.isOverData()) {
-				dataBlock.getDataBlockMap().put(header.getHeaderName(), getSingleDataBlockWithCells(field, object, header));
-			}
-			if (!header.isOverCollection() && header.isOverData()) {
-				dataBlock.getDataBlockMap().put(header.getHeaderName(), getSingleDataBlockWithOneCell(field, object, header));
-			}
-			if (header.isOverCollection() && !header.isOverData()) {
-				dataBlock.getDataBlockMap().put(header.getHeaderName(), getMultipleDataBlocksFromComplexObject(field, object, header));
-			}
-			if (!header.isOverCollection() && !header.isOverData()) {
-				dataBlock.getDataBlockMap().put(header.getHeaderName(), getSingleDataBlockFromComplexObject(field, object, header));
-			}
+			dataBlock.getInternalBlocks().put(header.getHeaderName(), getListOfDataBlocks(field, object, header));
 		}
 		return dataBlock;
+	}
+
+	private List<DataBlock> getListOfDataBlocks(Field field, Object object, Header header) {
+		switch (DataBlockType.getTypeByHeader(header)) {
+			case SINGLE_WITH_CELL:
+				return getSingleDataBlockWithCell(field, object, header);
+			case SINGLE_FROM_COMPLEX_OBJECT:
+				return getSingleDataBlockFromComplexObject(field, object, header);
+			case SINGLE_WITH_MULTIPLE_CELLS:
+				return getSingleDataBlockWithMultipleCells(field, object, header);
+			case MULTIPLE_FROM_COMPLEX_OBJECT:
+				return getMultipleDataBlocksFromComplexObject(field, object, header);
+			default:
+				throw new DataBlockCreateFail(Messages.NO_VALID_TYPE);
+		}
 	}
 
 	private void checkHeadersAndFieldsNumberEquality(Integer fieldsSize, Integer headersSize) {
@@ -84,12 +93,12 @@ public class DataBlockCreator {
 				.collect(Collectors.toList());
 	}
 
-	private List<DataBlock> getSingleDataBlockWithOneCell(Field field, Object object, Header header) {
+	private List<DataBlock> getSingleDataBlockWithCell(Field field, Object object, Header header) {
 		Object o = getObjectFromField(field, object);
 		return Collections.singletonList(new DataBlock(header, Collections.singletonList(new Cell(o))));
 	}
 
-	private List<DataBlock> getSingleDataBlockWithCells(Field field, Object object, Header header) {
+	private List<DataBlock> getSingleDataBlockWithMultipleCells(Field field, Object object, Header header) {
 		Collection collection = (Collection) getObjectFromField(field, object);
 		List<Cell> cells = (List<Cell>) collection.stream()
 				.map(o -> new Cell(o))
